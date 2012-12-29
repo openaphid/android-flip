bef17096d3eef18f412916b3c928ca81438c498f
Open Aphid <openaphid@gmail.com>
2012-12-29 16:36:31 +0800

    Migrate code from https://github.com/cagkanciloglu/FlipViewProblematicLayout #5

--- FlipView/Demo/src/com/aphidmobile/flip/demo/Issue5Activity.java
+++ FlipView/Demo/src/com/aphidmobile/flip/demo/Issue5Activity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 import com.aphidmobile.flip.FlipViewController;
 import com.aphidmobile.flip.demo.data.Travels;
+import com.aphidmobile.flip.demo.issue5.GalleryFlipAdapter;
 import com.aphidmobile.flipview.demo.R;
 import com.aphidmobile.utils.AphidLog;
 import com.aphidmobile.utils.IO;
@@ -32,7 +33,7 @@ public class Issue5Activity extends Activity {
 
 		flipView = new FlipViewController(this);
 
-		flipView.setAdapter(new MyBaseAdapter(this));
+		flipView.setAdapter(new GalleryFlipAdapter(this, flipView));
 
 		setContentView(flipView);
 	}
@@ -47,61 +48,5 @@ public class Issue5Activity extends Activity {
 	protected void onPause() {
 		super.onPause();
 		flipView.onPause();
-	}
-
-	private static class MyBaseAdapter extends BaseAdapter {
-
-		private LayoutInflater inflater;
-
-		private MyBaseAdapter(Context context) {
-			inflater = LayoutInflater.from(context);
-		}
-
-		@Override
-		public int getCount() {
-			return Travels.IMG_DESCRIPTIONS.size();
-		}
-
-		@Override
-		public Object getItem(int position) {
-			return position;
-		}
-
-		@Override
-		public long getItemId(int position) {
-			return position;
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View layout = convertView;
-			if (convertView == null)
-				layout = inflater.inflate(R.layout.issue5, null);
-
-			final Travels.Data data = Travels.IMG_DESCRIPTIONS.get(position);
-			
-			UI
-			.<TextView>findViewById(layout, R.id.gallery_flip_item_place_name_textview)
-			.setText(AphidLog.format("%d. %s", position, data.title));
-			
-			UI
-			.<ImageView>findViewById(layout, R.id.gallery_flip_item_background_imageview)
-			.setImageBitmap(IO.readBitmap(inflater.getContext().getAssets(), data.imageFilename));
-			
-			UI
-			.<TextView>findViewById(layout, R.id.gallery_flip_item_place_description_textview)
-			.setText(Html.fromHtml(data.description));
-			
-			UI
-			.<TextView>findViewById(layout, R.id.gallery_flip_item_place_city_textview)
-			.setText(data.city);
-			
-			UI
-			.<TextView>findViewById(layout, R.id.gallery_flip_item_place_country_textview)
-			.setText(data.country);
-			
-			return layout;
-		}
-
 	}
 }
--- FlipView/Demo/src/com/aphidmobile/flip/demo/MainActivity.java
+++ FlipView/Demo/src/com/aphidmobile/flip/demo/MainActivity.java
@@ -72,9 +72,10 @@ public class MainActivity extends ListActivity {
 		addItem(data, "Flip Async Content", FlipAsyncContentActivity.class);
 		addItem(data, "Flip with Event Listener", FlipTextViewAltActivity.class);
 		addItem(data, "Flip Horizontal", FlipHorizontalLayoutActivity.class);
-		addItem(data, "Flip XML", FlipTextViewXmlActivity.class);
-		addItem(data, "Fragment Demo", FlipFragmentActivity.class);
 		addItem(data, "Issue #5", Issue5Activity.class);
+		addItem(data, "XML Configuration", FlipTextViewXmlActivity.class);
+		addItem(data, "Fragment Demo", FlipFragmentActivity.class);
+		
 		return data;
 	}
 
