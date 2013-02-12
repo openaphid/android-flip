919e7fe35dbea5650b5a5b1831455d3d748fc64d
Aphid Mobile <aphidmobile@gmail.com>
2013-02-12 18:25:50 +0800

    fix some trivial issues

--- FlipView/Demo/src/com/aphidmobile/flip/demo/MainActivity.java
+++ FlipView/Demo/src/com/aphidmobile/flip/demo/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends ListActivity {
     List<Map<String, Object>> data = new ArrayList<Map<String, Object>>();
     addItem(data, "TextViews", FlipTextViewActivity.class);
     addItem(data, "Buttons", FlipButtonActivity.class);
-    addItem(data, "Complex Layouts", FlipDeleteAdapterActivity.class);
+    addItem(data, "Complex Layouts", FlipComplexLayoutActivity.class);
     addItem(data, "Async Content", FlipAsyncContentActivity.class);
     addItem(data, "Event Listener", FlipTextViewAltActivity.class);
     addItem(data, "Horizontal", FlipHorizontalLayoutActivity.class);
